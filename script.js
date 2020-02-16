3a36f8563bb6d06715ebad7e1e6d8776ff61d1c4
SpaceOfVariations <alecsandr.tiurin@gmail.com>
2020-02-16 19:05:17 +0200

    lesson19

---
+++ script.js
@@ -0,0 +1,23 @@
+var blue = document.getElementById("blue")
+
+blue.onclick = function() {
+    var sum = document.getElementById("def");
+    var text = 'You clicked First Buttom';
+    sum.innerHTML = text;
+}
+
+var green = document.getElementById("green")
+
+green.onclick = function() {
+    var sum = document.getElementById("def");
+    var text = 'You clicked Second Buttom';
+    sum.innerHTML = text;
+}
+
+var red = document.getElementById("red")
+
+red.onclick = function() {
+    var sum = document.getElementById("def");
+    var text = 'You clicked Third Buttom';
+    sum.innerHTML = text;
+}
